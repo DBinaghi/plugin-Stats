2c2b665fb457de15cac32a5d8528f7ced89b4626
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2026-03-18 11:13:52 +0100

    Changed for lists to select box (standard for quick-filter)

--- views/shared/common/quick-filters.php
+++ views/shared/common/quick-filters.php
@@ -1,41 +1,40 @@
-<ul class="quick-filter-wrapper">
-    <li><a href="#" tabindex="0"><?php echo __('Quick Filter'); ?></a>
-    <ul class="dropdown">
-        <li><span class="quick-filter-heading"><?php echo __('Quick Filter') ?></span></li>
 <?php
 $base_url = 'stats/browse/by-' . $stats_type;
-switch ($stats_type):
+$current  = html_escape(current_url());
+?>
+<select name="quick-filter" class="quick-filter" aria-label="<?php echo __('Quick Filter'); ?>"
+        onchange="if(this.value) window.location=this.value;">
+    <option><?php echo __('Quick Filter'); ?></option>
+<?php switch ($stats_type):
     case 'page': ?>
-        <li><a href="<?php echo url($base_url); ?>"><?php echo __('View All') ?></a></li>
-        <li><a href="<?php echo url($base_url, array('has_record' => true)); ?>"><?php echo __('With record') ?></a></li>
-        <li><a href="<?php echo url($base_url, array('has_record' => false)); ?>"><?php echo __('Without record') ?></a></li>
-        <?php break;
+    <option value="<?php echo url($base_url); ?>"><?php echo __('View All'); ?></option>
+    <option value="<?php echo url($base_url, array('has_record' => true)); ?>"><?php echo __('With record'); ?></option>
+    <option value="<?php echo url($base_url, array('has_record' => false)); ?>"><?php echo __('Without record'); ?></option>
+    <?php break;
 
     case 'record': ?>
-        <li><a href="<?php echo url($base_url); ?>"><?php echo __('View All') ?></a></li>
-        <li><a href="<?php echo url($base_url, array('record_type' => 'Item')); ?>"><?php echo __('By Item'); ?></a></li>
-        <li><a href="<?php echo url($base_url, array('record_type' => 'Collection')); ?>"><?php echo __('By Collection'); ?></a></li>
-        <li><a href="<?php echo url($base_url, array('record_type' => 'File')); ?>"><?php echo __('By File'); ?></a></li>
-        <?php if (plugin_is_active('SimplePages')): ?>
-        <li><a href="<?php echo url($base_url, array('record_type' => 'SimplePagesPage')); ?>"><?php echo __('By Simple Page'); ?></a></li>
-        <?php endif; ?>
-        <?php if (plugin_is_active('ExhibitBuilder')): ?>
-        <li><a href="<?php echo url($base_url, array('record_type' => 'Exhibit')); ?>"><?php echo __('By Exhibit'); ?></a></li>
-        <li><a href="<?php echo url($base_url, array('record_type' => 'ExhibitPage')); ?>"><?php echo __('By Exhibit Page'); ?></a></li>
-        <?php endif; ?>
-        <?php break;
+    <option value="<?php echo url($base_url); ?>"><?php echo __('View All'); ?></option>
+    <option value="<?php echo url($base_url, array('record_type' => 'Item')); ?>"><?php echo __('By Item'); ?></option>
+    <option value="<?php echo url($base_url, array('record_type' => 'Collection')); ?>"><?php echo __('By Collection'); ?></option>
+    <option value="<?php echo url($base_url, array('record_type' => 'File')); ?>"><?php echo __('By File'); ?></option>
+    <?php if (plugin_is_active('SimplePages')): ?>
+    <option value="<?php echo url($base_url, array('record_type' => 'SimplePagesPage')); ?>"><?php echo __('By Simple Page'); ?></option>
+    <?php endif; ?>
+    <?php if (plugin_is_active('ExhibitBuilder')): ?>
+    <option value="<?php echo url($base_url, array('record_type' => 'Exhibit')); ?>"><?php echo __('By Exhibit'); ?></option>
+    <option value="<?php echo url($base_url, array('record_type' => 'ExhibitPage')); ?>"><?php echo __('By Exhibit Page'); ?></option>
+    <?php endif; ?>
+    <?php break;
 
     case 'download': ?>
-        <li><a href="<?php echo url($base_url); ?>"><?php echo __('View All') ?></a></li>
-        <?php break;
+    <option value="<?php echo url($base_url); ?>"><?php echo __('View All'); ?></option>
+    <?php break;
 
     case 'field': ?>
-        <li><a href="<?php echo url($base_url, array('field' => 'referrer')); ?>"><?php echo __('Referrers'); ?></a></li>
-        <li><a href="<?php echo url($base_url, array('field' => 'query')); ?>"><?php echo __('Queries'); ?></a></li>
-        <li><a href="<?php echo url($base_url, array('field' => 'accept_language')); ?>"><?php echo __('Languages'); ?></a></li>
-        <li><a href="<?php echo url($base_url, array('field' => 'user_agent')); ?>"><?php echo __('Browsers'); ?></a></li>
-        <?php break;
+    <option value="<?php echo url($base_url, array('field' => 'referrer')); ?>"><?php echo __('Referrers'); ?></option>
+    <option value="<?php echo url($base_url, array('field' => 'query')); ?>"><?php echo __('Queries'); ?></option>
+    <option value="<?php echo url($base_url, array('field' => 'accept_language')); ?>"><?php echo __('Languages'); ?></option>
+    <option value="<?php echo url($base_url, array('field' => 'user_agent')); ?>"><?php echo __('Browsers'); ?></option>
+    <?php break;
 endswitch; ?>
-    </ul>
-    </li>
-</ul>
+</select>
